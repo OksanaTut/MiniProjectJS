f4f3446cf2163d1762f449f700a141b0a87779d0
OksanaTut <Sokolenko.o57@gmail.com>
2023-03-18 19:03:02 -0400

    Done. Check

--- MyProject/PostsMain.js
+++ MyProject/PostsMain.js
@@ -3,17 +3,6 @@
 // 8 Нижчє інформаці про пост, вивести всі коментарі поточного поста
 // (ендпоінт  - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
 
-// Стилизація проєкта -
-// index.html - всі блоки з user - по 2 в рядок. кнопки/аосилвння розташувати під інформацією про user.
-//
-// 	user-details.html - блок з інфою про user зверху сторінки. Кнопка нижчє, на 90% ширини сторінки, по центру.
-// 	блоки з короткою іфною про post - в ряд по 5 .
-//
-// 	post-details.html - блок з інфою про пост зверху. Коментарі - по 4 в ряд.
-//
-// 	Всі елементи котрі характеризують users, posts, comments візуалізувати, так, щоб було видно що це блоки
-// 	(дати фон. марджини і тд)
-
 let url=new URL(location.href);
 let id=url.searchParams.get('Id');
 
@@ -31,7 +20,6 @@ fetch('https://jsonplaceholder.typicode.com/posts/'+id)
 			.then(value => value.json())
 			.then(comments=>{
 				let divComments=document.createElement('div');
-				// divComments.innerText='comments of this post';
 				divComments.classList.add('listOfComments');
 				for (const comment of comments) {
 					let divComment = document.createElement('div');
